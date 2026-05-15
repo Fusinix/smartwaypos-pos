9abe66c6b6df6dbe09e47f8769299d222599f098
fusinix <team.enfusionx@gmail.com>
2026-05-15 16:16:15 +0000

    Fix2 - fix app crash startup

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import obfuscator from 'vite-plugin-javascript-obfuscator'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    obfuscator({
-      options: {
-        compact: true,
-        controlFlowFlattening: false,
-        deadCodeInjection: false,
-        identifierNamesGenerator: 'hexadecimal',
-        log: false,
-        numbersToExpressions: false,
-        renameGlobals: false,
-        selfDefending: false,
-        simplify: true,
-        splitStrings: true,
-        splitStringsChunkLength: 10,
-        stringArray: true,
-        stringArrayCallsTransform: true,
-        stringArrayEncoding: ['base64'],
-        stringArrayThreshold: 0.75,
-        transformObjectKeys: false,
-        unicodeEscapeSequence: false,
-      },
-    }),
   ],
   resolve: {
     alias: {
